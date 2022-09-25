f0ffa74aabfd22104e3342d4bf0a3c85dea7b965
deepaksy <deepaksy28@gmail.com>
2022-09-25 12:12:54 +0530

    Added Rating component

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -4,6 +4,7 @@ import { Button,
   TextField } from '@mui/material'
 import{ FormEvent, useState } from 'react'
 import axios from 'axios'
+import RadioGroupRating from '../components/StyledRating'
 
 export type FeedbackFormData={
   name:string,
@@ -30,21 +31,46 @@ const Feedback = () => {
     setFormData({...formData,[e.target.name]:e.target.value})
   }
   return (
-    <Box as="div">
+    <Box as="div" sx={{
+      display:'flex',
+      flexDirection:'column',
+      minHeight:'100vh',
+      position:'relative',
+      _before:{
+        content:'" "',
+        height:'24px',
+        flexGrow:1,
+        display:'block'
+      },
+      _after:{
+        content:'" "',
+        height:'24px',
+        flexGrow:1,
+        display:'block'
+      }
+    }}>
       {!formStatus?
         <Box as={'form'} sx={{
           display:'flex',
           flexDirection:'column',
-          maxWidth:['80%'],
-          margin:'auto'
+          justifyContent:'space-between',
+          width:['98%','80%'],
+          margin:'auto',
+          flexGrow:1,
+          borderStyle:'solid',
+          borderWidth:'thin',
+          borderColor:'inherit',
+          borderRadius:10,
+          padding:[5,12],
         }} 
         onSubmit={(e:FormEvent)=>handleSubmit(e)}>
           <TextField id='name' name="name" type="text" value={name} onChange={(e)=>handleChange(e)} aria-label='name' placeholder='' label='name' variant='standard'/>
           <TextField id='email' name="email" type='email' value={email} onChange={(e)=>handleChange(e)} aria-label='email' label='email' placeholder='ex: abc@gmail.com' variant='standard'/>
-          <TextField id='message' name="message" type="text" value={message} onChange={(e)=>handleChange(e)} aria-label='message' label='message' variant='standard'/>
+          <TextField sx={{minHeight:'20px'}} id='message' name="message" type="text" value={message} onChange={(e)=>handleChange(e)} aria-label='message' label='message' variant='standard'/>
           <Button type="submit">Submit</Button>
         </Box>
       :<h1>Thanks for the Feedback!</h1>}
+      <RadioGroupRating/>
     </Box>
   )
 }
